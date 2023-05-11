7560420ce1cfcfbdc1035cf37a6978636695d67a
xuanthu2002 <tranxuanthu3721@gmail.com>
2023-05-11 09:06:07 +0700

    .

--- code/frontend/src/components/Order/OrderItem/OrderItem.js
+++ code/frontend/src/components/Order/OrderItem/OrderItem.js
@@ -1,17 +1,42 @@
-const OrderItem = ({id, createTime, totalAmount, detailOrders}) => {
+import { Box, Button, Flex, Image, Input, Td, Tr } from '@chakra-ui/react';
+import format from '../../../format/currencyFormat.js';
+import moment from 'moment/moment.js';
+
+const OrderItem = ({ id, createTime, totalAmount, paymentMethod, status, detailOrders }) => {
     return (
-        <div>
-            <h2>Order ID: {id}</h2>
-            <p>Time order: {createTime}</p>
-            <p>Total Amount: {totalAmount}</p>
-            <ul>
-                {detailOrders.map((detailOrder) => (
-                    <li key={detailOrder.id}>
-                        {detailOrder.product.name} - {detailOrder.product.price} - {detailOrder.quantity}
-                    </li>
-                ))}
-            </ul>
-        </div>
+        // <div>
+        //     <h2>Order ID: {id}</h2>
+        //     <p>Time order: {createTime}</p>
+        //     <p>Total Amount: {totalAmount}</p>
+        //     <p>Payment Method: {paymentMethod}</p>
+        //     <p>Status: {status}</p>
+        //     <ul>
+        //         {detailOrders.map((detailOrder) => (
+        //             <li key={detailOrder.id}>
+        //                 {detailOrder.product.name} - {detailOrder.product.price} - {detailOrder.quantity}
+        //             </li>
+        //         ))}
+        //     </ul>
+        // </div>
+        <Tr>
+            <Td>
+                <Flex gap={'12px'} align='center'>
+                    {id}
+                </Flex>
+            </Td>
+            <Td>
+                {moment(createTime).format('DD/MM/YYYY, h:mm:ss a')}
+            </Td>
+            <Td>
+                {format.format(totalAmount)}
+            </Td>
+            <Td>
+                {paymentMethod}
+            </Td>
+            <Td>
+                {status}
+            </Td>
+        </Tr>
     );
 };
 export default OrderItem;
